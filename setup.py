3a35a3efca3b2c60b18a1a7cb849f18bc5ad0b84
Viraj-005 <124769627+Viraj-005@users.noreply.github.com>
2025-03-16 23:49:48 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generatve AI Poject',
+    version= '0.0.0',
+    author= 'Viraj Induruwa',
+    author_email='virajinduruwa2@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
